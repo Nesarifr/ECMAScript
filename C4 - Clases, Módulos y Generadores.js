2a7f64e179559c533b11993a44d9524c316aea5e
nesarifr <nestorarielfrias@gmail.com>
2022-01-22 22:09:53 -0300

    Correcciones apuntes C4

--- C4 - Clases, Módulos y Generadores.js
+++ C4 - Clases, Módulos y Generadores.js
@@ -1,9 +1,8 @@
-
+//<---------------------clases ------------------->
 class Calculator { //asigno el nombre Calculator // los clases no siguen el Hoisting // se debe iniciar siempre con mayuscula
     constructor(){ //asignamos un constructor en el bloque de codigo, es la manera de declarar una clase
-        this.valueA = 0; //asignamos las variables que van a estar 
-        this.valueB= 0; //dentro mi Scope global (local de la clase)
-
+        this.valueA = 0; //asignamos las variables que van a estar -->
+        this.valueB= 0; //DISPONIBLES EN UN Scope global
     }
     sum(valueA,valueB){ // genero este metodo para sumar
         this.valueA = valueA; //valores usados dentro de mi metodo, seran igualados a los valores recibidos
@@ -16,16 +15,17 @@ const calc=new Calculator(); //genero una constante, sera igual a la clase q aca
 //las clases se inicializan con new
 console.log(calc.sum(2,2)); //uso calc con el metodo y poniendo sus valores
 
+//<---------------------IMPORTAR ------------------->
 
 /* genero un arhivo "module.js"
 const hello = () =>{
     return 'hello!'
 } */
 
-import {hello} from './module.js
+import {hello} from './module.js'
 hello(); //en nodejs no funciona en terminal
 
-// <------------ GENERATOR --------->
+// <--------------------- GENERATOR -------------------->
 // es una funcion especial la cual retorna  una serie de valores segun el algoritmo definidos
 
 function* helloWorld(){  //sintaxis * //
