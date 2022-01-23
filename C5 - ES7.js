2fa11fca66506236c321bbe4642c1542e68ef5c4
nesarifr <nestorarielfrias@gmail.com>
2022-01-22 23:13:41 -0300

    C5 ES7 INCLUDES y EXPONENCIALES

---
+++ C5 - ES7.js
@@ -0,0 +1,16 @@
+//este metodo INCLUDES , trabaja sobre arreglos o string
+//nos permitira saber si hay un elemento dentro de estos
+
+let number = [1,2,3,7,8]; //hago un arreglo x
+
+if (number.includes(7)){ // busque un valor
+    console.log('si se encuentra el valor 7'); //dara que se encuentra
+} else {
+    console.log('no se encuentra')
+}
+
+// como elevar a la potencia (**)
+let base = 4;
+let exponent=3;
+let result=base**exponent;
+console.log(result);
